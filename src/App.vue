724262f35e736c744b999328a1c94ce4446fee35
YeHan <Mr.yehan@outlook.com>
2023-05-24 19:51:19 +0800

    添加公安备案链接。

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <el-container>
     <el-header>
-      <el-affix >
-        <el-menu background-color="#121212" :default-active="activeIndex" :ellipsis="false" router class="el-menu" mode="horizontal">
+      <el-affix>
+        <el-menu background-color="#121212" :default-active="activeIndex" :ellipsis="false" router class="el-menu"
+          mode="horizontal">
           <el-col :span="2"></el-col>
           <el-image style="width: 160px; height: 50px" :src="'logo1.png'" :fit="'contain'" />
           <el-col :span="15"></el-col>
@@ -16,10 +17,22 @@
       <el-backtop style="box-shadow: var(--el-box-shadow-dark); width: 60px; height: 60px;" :right="380" :bottom="200" />
     </el-main>
     <el-footer style="text-align: center;">
-      <div class="dashed" />
-      <br/>
-      © 2023-2024 YeHan.cc | 
-      <a href="https://beian.miit.gov.cn/" target="_blank">蜀ICP备2023011372号-1</a>
+      <p style="color:#939393;">© 2023-2024 YeHan.cc</p>
+      <a target="_blank" href="https://beian.miit.gov.cn/"
+        style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="" style="float:left;" />
+        <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">蜀ICP备2023011372号-1
+        </p>
+      </a>
+      <div style="width:300px;margin:0 auto; padding:20px 0;">
+        <img src="/beian.png">
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32011502011855"
+          style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src=""
+            style="float:left;" />
+          <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">苏公网安备 32011502011855号
+          </p>
+        </a>
+      </div>
+
     </el-footer>
   </el-container>
 </template>
